ab56a5957bbb535e5d491ac06a8f08fb02da955b
TomH <t.ham@liverpool.ac.uk>
2021-09-10 10:13:22 -0500

    Feedback based changes; address negative energy + stylistic comments etc.

--- larpandora/LArPandoraEventBuilding/LArPandoraShower/Tools/ShowerESTAREnergy_tool.cc
+++ larpandora/LArPandoraEventBuilding/LArPandoraShower/Tools/ShowerESTAREnergy_tool.cc
@@ -44,7 +44,6 @@ namespace ShowerRecoTools {
            const geo::PlaneID::PlaneID_t plane);
 
   art::InputTag fPFParticleLabel;
-  int fVerbose;
 
   //Services
   art::ServiceHandle<geo::Geometry> fGeom;
@@ -55,12 +54,15 @@ namespace ShowerRecoTools {
   std::string fShowerBestPlaneOutputLabel; 
   bool fSCECorrectEField;
 
-  double nominal_Efield = 0;         // Nominal E-field in the detector
-  double localEfield = 0;            // Local E-field at the geometric centre of the shower 
-  double localEfield_cweighted = 0;  // Local E-field at the charge weighted centre of the shower
+  double fNominal_Efield;          // Nominal E-field in the detector
+  double fLocal_Efield;            // Local E-field at the geometric centre of the shower 
+  double fLocal_Efield_cweighted;  // Local E-field at the charge weighted centre of the shower
 
   std::vector<std::tuple<int, double>> sp_hits_tuple; // tuple holding the key and local E-field for all the sp hits 
 
+  std::string fname;
+  std::string ESTARPath;
+  std::string ESTAR_TGraph_name;
   TGraph2D *t_estar;
 
   }; // class
@@ -68,22 +70,22 @@ namespace ShowerRecoTools {
   ShowerESTAREnergy::ShowerESTAREnergy(const fhicl::ParameterSet& pset) :
     IShowerTool(pset.get<fhicl::ParameterSet>("BaseTools")),
     fPFParticleLabel(pset.get<art::InputTag>("PFParticleLabel")),
-    fVerbose(pset.get<int>("Verbose")),
     fCalorimetryAlg(pset.get<fhicl::ParameterSet>("CalorimetryAlg")),
     fShowerEnergyOutputLabel(pset.get<std::string>("ShowerEnergyOutputLabel")),
     fShowerBestPlaneOutputLabel(pset.get<std::string>("ShowerBestPlaneOutputLabel")),
-    fSCECorrectEField(pset.get<bool>("SCECorrectEField"))
+    fSCECorrectEField(pset.get<bool>("SCECorrectEField")),
+    fNominal_Efield(0),
+    fLocal_Efield(0),
+    fLocal_Efield_cweighted(0),
+    ESTARPath(pset.get<std::string>("ESTARFname")),
+    ESTAR_TGraph_name(pset.get<std::string>("ESTARTGraph"))
   {
 
     //Get the ESTAR lookup curve file name
-    std::string fname;
     cet::search_path sp("FW_SEARCH_PATH");
-    auto ESTARPath = pset.get<std::string>("ESTARFname");
     if (!sp.find_file(ESTARPath, fname)) {
       throw cet::exception("ShowerESTAREnergy") << "Could not find the ESTAR lookup curve file.";
     }
-
-    std::string ESTAR_TGraph_name = pset.get<std::string>("ESTARTGraph");
 
     TFile fin(fname.c_str(), "READ");
     if (!fin.IsOpen()) {
@@ -106,55 +108,53 @@ namespace ShowerRecoTools {
         reco::shower::ShowerElementHolder& ShowerEleHolder
   ){
 
-    if(fVerbose){
-      std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Shower ESTAR Energy Tool ~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
-      if(fSCECorrectEField){std::cout << "Applying SCE correction" << std::endl;}
-    }
-      
+    mf::LogInfo("ShowerESTAREnergy") << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Shower ESTAR Energy Tool ~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+    if(fSCECorrectEField){mf::LogVerbatim("ShowerESTAREnergy") << "Applying SCE correction" << std::endl;}
+
     // Get the associated pfParicle vertex PFParticles
-    auto const pfpHandle = Event.getValidHandle<std::vector<recob::PFParticle>>(fPFParticleLabel);
+    auto const pfpHandle{Event.getValidHandle<std::vector<recob::PFParticle>>(fPFParticleLabel)};
 
     // Get the clusters
-    auto const clusHandle = Event.getValidHandle<std::vector<recob::Cluster>>(fPFParticleLabel);
+    auto const clusHandle{Event.getValidHandle<std::vector<recob::Cluster>>(fPFParticleLabel)};
 
-    const art::FindManyP<recob::Cluster>& fmc = ShowerEleHolder.GetFindManyP<recob::Cluster>(pfpHandle, Event, fPFParticleLabel);
-    std::vector<art::Ptr<recob::Cluster> > clusters = fmc.at(pfparticle.key());
+    const art::FindManyP<recob::Cluster>& fmc{ShowerEleHolder.GetFindManyP<recob::Cluster>(pfpHandle, Event, fPFParticleLabel)};
+    std::vector<art::Ptr<recob::Cluster>> clusters{fmc.at(pfparticle.key())};
 
     // Get the hits association from the clusters
-    const art::FindManyP<recob::Hit>& fmhc = ShowerEleHolder.GetFindManyP<recob::Hit>(clusHandle, Event, fPFParticleLabel);
+    const art::FindManyP<recob::Hit>& fmhc{ShowerEleHolder.GetFindManyP<recob::Hit>(clusHandle, Event, fPFParticleLabel)};
     
     std::map<geo::PlaneID::PlaneID_t, std::vector<art::Ptr<recob::Hit>>> planeHits;
 
     // Get the spacepoints
-    auto const spHandle = Event.getValidHandle<std::vector<recob::SpacePoint>>(fPFParticleLabel);
+    auto const spHandle{Event.getValidHandle<std::vector<recob::SpacePoint>>(fPFParticleLabel)};
 
-    const art::FindManyP<recob::SpacePoint>& fmsp = ShowerEleHolder.GetFindManyP<recob::SpacePoint>(pfpHandle, Event, fPFParticleLabel);
-    std::vector<art::Ptr<recob::SpacePoint> > spacepoints = fmsp.at(pfparticle.key());
+    const art::FindManyP<recob::SpacePoint>& fmsp{ShowerEleHolder.GetFindManyP<recob::SpacePoint>(pfpHandle, Event, fPFParticleLabel)};
+    std::vector<art::Ptr<recob::SpacePoint>> spacepoints{fmsp.at(pfparticle.key())};
 
     // Get the hits association from the spacepoints
-    const art::FindManyP<recob::Hit>& fmhsp = ShowerEleHolder.GetFindManyP<recob::Hit>(spHandle, Event, fPFParticleLabel);
+    const art::FindManyP<recob::Hit>& fmhsp{ShowerEleHolder.GetFindManyP<recob::Hit>(spHandle, Event, fPFParticleLabel)};
 
-    auto const clockData = art::ServiceHandle<detinfo::DetectorClocksService const>()->DataFor(Event);
-    auto const detProp   = art::ServiceHandle<detinfo::DetectorPropertiesService const>()->DataFor(Event, clockData);
+    auto const clockData{art::ServiceHandle<detinfo::DetectorClocksService const>()->DataFor(Event)};
+    auto const detProp{art::ServiceHandle<detinfo::DetectorPropertiesService const>()->DataFor(Event, clockData)};
     
     // Get E-field -  Electric Field in the drift region in KV/cm
-    nominal_Efield  = detProp.Efield(); // Nominal E-field 
+    fNominal_Efield = detProp.Efield(); // Nominal E-field 
    
     // Not all hits from the clusters will match to Space points. If that's the case let's use some sort of shower centre
     // Get shower centre 
-    TVector3 showercentre = IShowerTool::GetLArPandoraShowerAlg().ShowerCentre(spacepoints);
+    const TVector3 showercentre{IShowerTool::GetLArPandoraShowerAlg().ShowerCentre(spacepoints)};
     
     // Get the charge weighted shower centre - sum(charge*position)/sum(charge)
     // Charge is obtained from the lifetime corrected hits and hits are obtained from the spacepoints 
-    TVector3 chargecentre = IShowerTool::GetLArPandoraShowerAlg().ShowerCentre(clockData, detProp, spacepoints, fmhsp);
+    const TVector3 chargecentre{IShowerTool::GetLArPandoraShowerAlg().ShowerCentre(clockData, detProp, spacepoints, fmhsp)};
     
     // Get space charge corrected E-field
     if(fSCECorrectEField){
       // Check the shower centre exists and is sensible
       // Think the charge weighted centre can take nan values which causes the E-field calculation to fail
-      if(showercentre.Mag() >= 0 && chargecentre.Mag() >=0){ 
-        localEfield = IShowerTool::GetLArPandoraShowerAlg().SCECorrectEField(nominal_Efield, showercentre);
-        localEfield_cweighted = IShowerTool::GetLArPandoraShowerAlg().SCECorrectEField(nominal_Efield, chargecentre);
+      if(!isnan(showercentre.Mag()) && !isnan(chargecentre.Mag())){ 
+        fLocal_Efield = IShowerTool::GetLArPandoraShowerAlg().SCECorrectEField(fNominal_Efield, showercentre);
+        fLocal_Efield_cweighted = IShowerTool::GetLArPandoraShowerAlg().SCECorrectEField(fNominal_Efield, chargecentre);
       }
       else{
         mf::LogWarning("ShowerESTAREnergy") << "Shower centre calculation doesn't look to be sensible. Reconstruction is probably dodgy." << std::endl;
@@ -165,9 +165,9 @@ namespace ShowerRecoTools {
     for(auto const& cluster: clusters){
           
       // Get the hits
-      std::vector<art::Ptr<recob::Hit> > hits = fmhc.at(cluster.key());
+      const std::vector<art::Ptr<recob::Hit>> hits{fmhc.at(cluster.key())};
       // Get the plane.
-      const geo::PlaneID::PlaneID_t plane(cluster->Plane().Plane);
+      const geo::PlaneID::PlaneID_t plane{cluster->Plane().Plane};
        
       planeHits[plane].insert(planeHits[plane].end(),hits.begin(),hits.end());
     }
@@ -176,11 +176,11 @@ namespace ShowerRecoTools {
     sp_hits_tuple.clear();
     for(auto const& spacepoint : spacepoints){
       //Get the hits
-      std::vector<art::Ptr<recob::Hit> > hits = fmhsp.at(spacepoint.key());
+      const std::vector<art::Ptr<recob::Hit>> hits{fmhsp.at(spacepoint.key())};
 
       // get local Efield at SP_pos
-      TVector3 sp_pos = IShowerTool::GetLArPandoraShowerAlg().SpacePointPosition(spacepoint); 
-      double localEfield_SP = IShowerTool::GetLArPandoraShowerAlg().SCECorrectEField(nominal_Efield, sp_pos);
+      TVector3 sp_pos{IShowerTool::GetLArPandoraShowerAlg().SpacePointPosition(spacepoint)}; 
+      const double localEfield_SP{IShowerTool::GetLArPandoraShowerAlg().SCECorrectEField(fNominal_Efield, sp_pos)};
 
       // Store the localEfield of all the hits with associated SP's so we can compare with the hits from the clusters later
       for(auto const& h : hits){
@@ -189,7 +189,7 @@ namespace ShowerRecoTools {
     }
 
     // Calculate the energy for each plane && best plane
-    geo::PlaneID::PlaneID_t bestPlane  = std::numeric_limits<geo::PlaneID::PlaneID_t>::max();
+    geo::PlaneID::PlaneID_t bestPlane{std::numeric_limits<geo::PlaneID::PlaneID_t>::max()};
     unsigned int bestPlaneNumHits = 0;
 
     //Holder for the final product
@@ -201,9 +201,9 @@ namespace ShowerRecoTools {
       unsigned int planeNumHits = hits.size();
         
       //Calculate the Energy
-      double Energy = CalculateEnergy(clockData, detProp, hits, plane);
+      const double Energy{CalculateEnergy(clockData, detProp, hits, plane)};
 
-      if(Energy > 0){    
+      if(Energy > std::numeric_limits<double>::epsilon()){    
         ShowerESTAREnergy.at(plane) = Energy;
       }
 
@@ -211,12 +211,11 @@ namespace ShowerRecoTools {
         bestPlane        = plane;
         bestPlaneNumHits = planeNumHits;
       }
-      if(fVerbose){
-        std::cout << "plane: " << plane
+        
+      mf::LogVerbatim("ShowerESTAREnergy") 
+                  << "plane: " << plane
                   << "  hits: " << hits.size()
                   << "  energy: " << Energy << std::endl;  
-      }
-
     }
 
     ShowerEleHolder.SetElement(ShowerESTAREnergy,ShowerESTAREnergyError,fShowerEnergyOutputLabel);
@@ -227,9 +226,7 @@ namespace ShowerRecoTools {
       // have to define a new variable as we pass-by-reference when filling
       int bestPlaneVal(bestPlane);
       ShowerEleHolder.SetElement(bestPlaneVal, fShowerBestPlaneOutputLabel);
-      if(fVerbose){
-        std::cout << "best plane: " << bestPlane << ",  best plane number of hit: " << bestPlaneNumHits << std::endl;
-      }
+      mf::LogVerbatim("ShowerESTAREnergy") << "best plane: " << bestPlane << ",  best plane number of hit: " << bestPlaneNumHits << std::endl; 
     }
      
     return 0;
@@ -241,39 +238,42 @@ namespace ShowerRecoTools {
          const detinfo::DetectorPropertiesData& detProp,
          const std::vector<art::Ptr<recob::Hit> >& hits, const geo::PlaneID::PlaneID_t plane){
    
-    double totalEnergy = 0;
-    double efield = 0; 
+    double totalEnergy{0};
+    double efield{0}; 
      
     // Loop over the hits
     for(auto const& h : hits){
       // If we're correcting for SCE, check if the hit is one that was we found to have an associated SP
       if(fSCECorrectEField){
-        auto it = std::find_if(sp_hits_tuple.begin(), sp_hits_tuple.end(), [&](const std::tuple<unsigned int, double>& e){
+        const auto it = std::find_if(sp_hits_tuple.begin(), sp_hits_tuple.end(), [&](const std::tuple<unsigned int, double>& e){
         return std::get<0>(e) == h.key();
-      });
+        });
       
-      // do a per hit SCE correction since we have associated space points
-      if(it != sp_hits_tuple.end()){
-        efield = std::get<1>(*it);
+        // do a per hit SCE correction since we have associated space points
+        if(it != sp_hits_tuple.end()){
+          efield = std::get<1>(*it);
+        }
+
+        // no associated space points so correct these hits using the shower centre position 
+        else{
+          efield = fLocal_Efield_cweighted;
+        }
       }
 
-      // no associated space points :( so correct these hits using the shower centre position 
       else{
-        efield = localEfield_cweighted;
+        efield = fNominal_Efield; 
       }
-    }
 
-    else{
-      efield = nominal_Efield; 
-    }
-
-    double totalCharge = (h)->Integral() * fCalorimetryAlg.LifetimeCorrection(clockData, detProp, (h)->PeakTime()); // obtain charge and correct for lifetime
-    double nElectrons = fCalorimetryAlg.ElectronsFromADCArea(totalCharge, plane);
-    if(nElectrons !=0 && t_estar->Interpolate(nElectrons, efield) <= 0){
-      mf::LogWarning("ShowerESTAREnergy") << "Interpolation is returning an energy of 0 even though we do have some collected charge. " 
-                                          << "Probably due to an out of bounds entry for the charge or the efield." << std::endl; 
-    }
-    totalEnergy += t_estar->Interpolate(nElectrons, efield);
+      const double totalCharge{(h)->Integral() * fCalorimetryAlg.LifetimeCorrection(clockData, detProp, (h)->PeakTime())}; // obtain charge and correct for lifetime
+      const double nElectrons{fCalorimetryAlg.ElectronsFromADCArea(totalCharge, plane)};
+      if(nElectrons !=0 && t_estar->Interpolate(nElectrons, efield) <= 0){
+        mf::LogWarning("ShowerESTAREnergy") << "Interpolation is returning an energy of 0 (or negative) even though we do have some collected charge. " 
+                                            << "Probably due to an out of bounds entry for the charge or the efield." << std::endl; 
+      }
+      // Make sure we aren't counting energy which is somehow negative
+      if(t_estar->Interpolate(nElectrons, efield) > 0){
+        totalEnergy += t_estar->Interpolate(nElectrons, efield);
+      }
 
     }
     return totalEnergy; 
@@ -281,13 +281,4 @@ namespace ShowerRecoTools {
 } // namespace
 
 DEFINE_ART_CLASS_TOOL(ShowerRecoTools::ShowerESTAREnergy)
-
-
-
-
-
-
-
-
-
 
